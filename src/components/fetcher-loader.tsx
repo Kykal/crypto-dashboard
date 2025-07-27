a8aec3965971bcbe052f6991cbe271ac2d419c5d
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2025-07-27 10:02:40 -0600

    refactor

--- src/components/fetcher-loader.tsx
+++ src/components/fetcher-loader.tsx
@@ -1,9 +1,10 @@
-import useSWR from "swr";
-import { useSearchParams } from "react-router";
-
-import useCryptosStore from "../store/use-cryptos-store";
 import { api, fetcher, SearchParam } from "../constants";
 import urlWithSearchParams from "../utils/urlWithSearchParams";
+
+//ReactJS
+import useSWR from "swr";
+import { useSearchParams } from "react-router";
+import useCryptosStore from "../store/use-cryptos-store";
 import { useEffect } from "react";
 
 /**
@@ -22,7 +23,12 @@ export default FetcherLoader; // Export main component
 
 const CryptoStoreFetcher = () => {
   const [searchParams] = useSearchParams();
-  const { data: cryptos, setData: setCryptos } = useCryptosStore();
+
+  const cryptosHistoryFrequencySearchParam = searchParams.get(
+    SearchParam.CRYPTOS_HISTORY_FREQUENCY
+  );
+
+  const setCryptos = useCryptosStore((state) => state.setData);
 
   const url = urlWithSearchParams(api.coins.markets.path, {
     vs_currency: "usd",
@@ -30,31 +36,26 @@ const CryptoStoreFetcher = () => {
     per_page: 10,
     page: 1,
     precision: 2,
-    price_change_percentage:
-      searchParams.get(SearchParam.CRYPTOS_HISTORY_FREQUENCY) ?? "24h",
+    price_change_percentage: cryptosHistoryFrequencySearchParam ?? "24h",
   });
 
   const { data, error, isLoading } = useSWR(url, fetcher);
 
   useEffect(() => {
     setCryptos({
-      data: [],
+      data: null,
       error: null,
       loading: true,
     });
-  }, [searchParams]);
+  }, [cryptosHistoryFrequencySearchParam]);
 
-  if (data && cryptos.length === 0) {
+  useEffect(() => {
     setCryptos({
-      data,
+      data: data ?? null,
       error,
       loading: isLoading,
     });
-  }
-
-  if (error) {
-    console.error(error);
-  }
+  }, [data, error, isLoading]);
 
   return null;
 };
