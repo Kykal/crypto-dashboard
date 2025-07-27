ddbafa0c467872d8acc3afebbe502b62fa2506fd
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2025-07-27 10:01:34 -0600

    add constant

--- src/constants.ts
+++ src/constants.ts
@@ -5,12 +5,26 @@ export const api = {
     markets: {
       path: `${API_BASE_URL}/coins/markets`,
     },
+    id: (id: string) => ({
+      path: `${API_BASE_URL}/coins/${id}`,
+      market_chart: {
+        path: `${API_BASE_URL}/coins/${id}/market_chart`,
+      },
+    }),
   },
 } as const;
 
 export const fetcher = (...args: any) =>
-  fetch(...args).then((res) => res.json());
+  fetch(...args, {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
+    },
+  }).then((res) => res.json());
 
 export enum SearchParam {
+  CRYPTO_HISTORY_FREQUENCY = "crypto-history-frequency",
   CRYPTOS_HISTORY_FREQUENCY = "cryptos-history-frequency",
+  CRYPTO = "crypto",
 }
