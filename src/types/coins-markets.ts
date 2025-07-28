5385e21753014716344ce760fa42c3f5f2975976
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2025-07-28 10:04:42 -0600

    add query types

--- src/types/coins-markets.ts
+++ src/types/coins-markets.ts
@@ -1,3 +1,109 @@
+export interface CoinMarketQueryParams {
+  /**
+   * target currency of coins and market data
+   *
+   * **refers to /simple/supported_vs_currencies
+   */
+  vs_currency: string;
+
+  /**
+   * coins' IDs, comma-separated if querying more than 1 coin.
+   *
+   * **refers to /coins/list
+   */
+  ids?: string;
+
+  /**
+   * coins’ names, comma-separated if querying more than 1 coin.
+   */
+  names?: string;
+
+  /**
+   * coins’ symbols, comma-separated if querying more than 1 coin.
+   */
+  symbols?: string;
+
+  /**
+   * for `symbols` lookups, specify `all` to include all matching tokens
+   *
+   * Default `top` returns top-ranked tokens (by market cap or volume)
+   */
+  include_tokens?: string;
+
+  /**
+   * filter based on coins’ category
+   *
+   * **refers to /coins/categories/list
+   */
+  category?: string;
+
+  /**
+   * sort result by field, default: market_cap_desc
+   */
+  order?:
+    | "market_cap_asc"
+    | "market_cap_desc"
+    | "volume_asc"
+    | "volume_desc"
+    | "id_asc"
+    | "id_desc";
+
+  /**
+   * total results per page, default: 100
+   *
+   * Valid values: 1..250
+   */
+  per_page?: number;
+
+  /**
+   * page through results, default: 1
+   */
+  page?: number;
+
+  /**
+   * include sparkline 7 days data, default: false
+   */
+  sparkline?: boolean;
+
+  /**
+   * include price change percentage timeframe, comma-separated if query more than 1 price change percentage timeframe
+   *
+   * Valid values: 1h, 24h, 7d, 14d, 30d, 200d, 1y
+   */
+  price_change_percentage?: string;
+
+  /**
+   * language background, default: en
+   */
+  locale?: string;
+
+  /**
+   * decimal place for currency price value
+   */
+  precision?:
+    | "full"
+    | 0
+    | 1
+    | 2
+    | 3
+    | 4
+    | 5
+    | 6
+    | 7
+    | 8
+    | 8
+    | 9
+    | 10
+    | 11
+    | 12
+    | 13
+    | 14
+    | 15
+    | 16
+    | 17
+    | 18;
+}
+
 export interface CoinMarket {
   id: string;
   symbol: string;
