9d08a5feb3aff971a056701105b8069f45e2038c
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2025-07-28 08:40:25 -0600

    add title

--- src/components/organisms/top-cryptos-with-positive-trend/index.tsx
+++ src/components/organisms/top-cryptos-with-positive-trend/index.tsx
@@ -1,7 +1,18 @@
+//MATERIAL DESIGN
+//Components
+
+import { Stack, Title } from "@mantine/core";
+
 //Main component
 const TopCryptosWithPositiveTrend = () => {
   //Main render
-  return <></>;
+  return (
+    <Stack>
+      <Title order={2} c="dimmed" size="md">
+        Top positive tendency cryptos
+      </Title>
+    </Stack>
+  );
 };
 
 export default TopCryptosWithPositiveTrend; // Export main component
