d351aee25807551ba79d77055869a85470db95d6
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2025-07-28 07:11:58 -0600

    add custom render option

--- src/components/organisms/crypto-history-chart/cryptos-select.tsx
+++ src/components/organisms/crypto-history-chart/cryptos-select.tsx
@@ -7,17 +7,20 @@ import useCryptosStore from "../../../store/use-cryptos-store";
 
 //MATERIAL DESIGN
 //Components
-import { Select } from "@mantine/core";
+import { Avatar, Group, Select } from "@mantine/core";
 
 //Types
-import type { ComboboxData } from "@mantine/core";
+import type { ComboboxData, SelectProps } from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
 
 //Main component
 const CryptosSelect = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const cryptos = useCryptosStore((store) => store.data);
+
   const data: ComboboxData =
-    useCryptosStore((state) => state.data)?.map((item) => ({
+    cryptos?.map((item) => ({
       label: item.name,
       value: item.id,
     })) ?? [];
@@ -40,8 +43,38 @@ const CryptosSelect = () => {
     setSearchParams(newSearchParams);
   };
 
+  const renderOptions: SelectProps["renderOption"] = ({ option, checked }) => (
+    <Group flex={1} align="center" justify="space-between">
+      {option.label}
+      <Group align="center" gap="xs">
+        {checked && (
+          <IconCheck style={{ marginInlineStart: "auto" }} {...iconProps} />
+        )}
+        <Avatar
+          src={cryptos?.find((item) => item.name === option.label)?.image ?? ""}
+          alt={option.label}
+          size={24}
+        />
+      </Group>
+    </Group>
+  );
+
   //Main render
-  return <Select value={value} onChange={onChangeHandler} data={data} />;
+  return (
+    <Select
+      value={value}
+      onChange={onChangeHandler}
+      data={data}
+      renderOption={renderOptions}
+    />
+  );
 };
 
 export default CryptosSelect; // Export main component
+
+const iconProps = {
+  stroke: 1.5,
+  color: "currentColor",
+  opacity: 0.6,
+  size: 18,
+};
